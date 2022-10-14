4c5c8d6141a8630a49efa1c13d88226a9c09d1c9
axelyvesi2@gmail.com <axelyvesi2@gmail.com>
2022-10-14 17:04:20 +0200

    FrontSpaceX_Docker

---
+++ src/pages/home.js
@@ -0,0 +1,19 @@
+
+const Home = () => {
+    return (<>
+
+        <div class="axel">
+            <h1>Front Space X</h1>
+        </div>
+
+        <div class="kol-1">
+            <ul>
+                <li><a href="/launches">Lunches 🧑‍🚀</a></li>
+                <li><a href="/capsules">Capsules 🚀</a></li>
+           </ul>
+        </div>
+    </>
+    )
+}
+
+export default Home;
